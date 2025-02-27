4cc0d62361bca6bde054215310dc46cbab81042d
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2025-02-27 19:05:52 +0200

    d

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           ))}
         </div>
         {/* Weekly Planner Section */}
-        <WeeklyPlanner />
+        {/* <WeeklyPlanner /> */}
       </main>
     </>
   );
